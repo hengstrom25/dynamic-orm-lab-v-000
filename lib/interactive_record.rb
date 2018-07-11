cd971904caab858692284f2084a8e0892d15e068
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-11 15:34:55 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,7 +53,7 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
   
-  def self.find_by(option)
+  def self.find_by(hash)
     sql = "SELECT * FROM #{self.table_name} WHERE option = '#{option}'"
     DB[:conn].execute(sql)
   end
